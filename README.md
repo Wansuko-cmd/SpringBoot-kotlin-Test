6281e5d18bed676b7ad97dbcb57f591be6259ffe
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2020-10-26 23:19:13 +0900

    docker-compose-ymlを変更

--- README.md
+++ README.md
@@ -1,14 +1,16 @@
 # SpringBoot-kotlin-Test
 <h1>利用方法</h1>
+<h3>初回</h3>
 <ul>
     <li>dockerファイルに移動する</li>
     <li>docker-compose.ymlの中のvolumesの値を書く環境に合わせて変更</li>
-    <li>コマンドで</li>
-    <li>$docker-compose up</li>
-    <li>を実行する。</li>
-    <li>以上</li>
+    <li>コマンドで $docker-compose up を実行する。</li>
 </ul>
+<h3>二回目以降</h3>
+<p>$docker ps -a　でコンテナ名を確認</p>
+<p>$docker start コンテナ名　を実行</p>
 
+以上
 
 
 
--- src/main/kotlin/com/example/setReact/SampleController.kt
+++ src/main/kotlin/com/example/setReact/SampleController.kt
@@ -1,6 +1,7 @@
 package com.example.setReact
 
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
@@ -9,4 +10,9 @@ class SampleController {
     fun mainController(): String{
         return "index"
     }
+
+    /*@GetMapping("/")
+    fun getId(): String{
+        return "Hello"
+    }*/
 }
