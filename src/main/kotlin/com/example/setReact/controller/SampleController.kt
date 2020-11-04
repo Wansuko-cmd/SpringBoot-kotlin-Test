29c9117bf247c0359aed0f32f500a58b0433fce6
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2020-11-05 00:46:45 +0900

    チュートリアルに沿って組み換え中

--- src/main/kotlin/com/example/setReact/controller/SampleController.kt
+++ src/main/kotlin/com/example/setReact/controller/SampleController.kt
@@ -1,20 +1,21 @@
 package com.example.setReact.controller
 
+import com.example.setReact.db.NameEntity
 import com.example.setReact.db.NameService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
-import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
 class SampleController {
     @Autowired
     private lateinit var service: NameService
 
-    @RequestMapping("/")
-    fun mainController(model: Model): String{
-        val test: String = service.getAll()[0].name
-        model.addAttribute("name", test)
+    @GetMapping("/")
+    fun index(model: Model): String{
+        val members: List<NameEntity> = service.getAll()
+        model.addAttribute("members", members)
         return "index"
     }
 }
